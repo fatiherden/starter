a87015e85178200f78316d6704daf782df43ecd3
='FatihE <='fatiherden85@gmail.com>
2021-10-14 12:31:35 +0300

    first commit

---
+++ src/test/java/com/cybertek/tests/FakerClass.java
@@ -0,0 +1,19 @@
+package com.cybertek.tests;
+//import com.github.javafaker.CreditCardType;
+//import com.github.javafaker.Faker;
+
+
+public class FakerClass {
+
+    public static void main(String[] args) {
+        //Faker faker = new Faker();
+        //
+        //System.out.println(faker.name().fullName());
+        //
+        //System.out.printl(faker.harryPotter().character());
+        //System.out.println(faker.finance().creditCard(CreditCardType.AMERICAN_EXPRESS));
+
+
+    }
+
+}
